99bdd9c3733a6ec7f5174e209d544c7ee5cbe894
bhagirathauti <bhagirath961778@gmail.com>
2023-11-02 18:21:58 +0530

    numeron completed

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Iteration 1: Make the Play button functional.
+const playbutton = document.getElementById("play-button")
+
+playbutton.onclick = ()=>{
+    window.location.href = "./game.html";
+}
 // Description: When the Play button is clicked the game page should be displayed.
+
+
--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const scoreBox = document.getElementById("score-board")
+let passcode = localStorage.getItem("squad-56");
+scoreBox.innerHTML = passcode;
+
+
+const playAgain = document.getElementById("play-again-button")
+playAgain.onclick = () =>{
+    window.location.href = "./game.html"
+}
